1c917b0b3d23a59bf5007f56e596f738e19c796b
牛良涛 <niuliangtao@weidian.com>
2018-02-05 23:38:21 +0800

    add

--- PersonLearn/src/main/com/competition/alibaba/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/10/19 下午7:53
-# @Author  : niuliangtao
-# @Site    : 
-# @File    : __init__.py.py
-# @Software: PyCharm
--- niult/__init__.py
+++ niult/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2018/02/02 20:43
+# @Time    : 2018/02/05 23:26
 # @Author  : niuliangtao
 # @Site    : 
 # @File    : __init__.py.py
